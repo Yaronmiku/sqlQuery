f685878d6e05368a96d3ff0394b01b1a31848b87
duy <60863634+Yaronmiku@users.noreply.github.com>
2023-04-12 22:00:16 +0700

    teacher want me code from scratch .. (after demo)

---
+++ SQLQueryDEMO.sql
@@ -0,0 +1,21 @@
+﻿/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [PlayerID]
+      ,[PlayerName]
+      ,[DoB]
+      ,[YellowCards]
+      ,[RedCards]
+      ,[SquadNumber]
+      ,[Position]
+      ,[ClubID]r
+  FROM [DEMO].[dbo].[Players]
+//Liệt kê tất cả CLB có cầu thủ nhận thẻ vàng 
+SELECT ClubName, PlayerName, YellowCards
+FROM Players
+JOIN Clubs
+ON Players.PlayerID = Clubs.ClubID
+Where YellowCards>0
+
+SELECT [Clubs].[ClubName], [Players].[PlayerName], [Players].[YellowCards]
+FROM ([soccer_match].[dbo].[Players]
+INNER JOIN [Clubs] ON [Players].[ClubID] = [Clubs].[ClubID])
+WHERE [YellowCards] > 0
